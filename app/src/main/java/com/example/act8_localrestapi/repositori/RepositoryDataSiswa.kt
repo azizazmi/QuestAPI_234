b7c0401255be814e9d429b10b392d8e8aab417df
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-12-18 09:30:04 +0700

    feat: add JaringanRepositoryDataSiswa implementation class in RepositoryDataSiswa file

--- app/src/main/java/com/example/act8_localrestapi/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/act8_localrestapi/repositori/RepositoryDataSiswa.kt
@@ -1,5 +1,6 @@
 package com.example.act8_localrestapi.repositori
 
+import com.example.act8_localrestapi.apiservice.ServiceApiSiswa
 import com.example.act8_localrestapi.modeldata.DataSiswa
 
 interface RepositoryDataSiswa {
@@ -7,3 +8,9 @@ interface RepositoryDataSiswa {
     suspend fun postDataSiswa(dataSiswa: DataSiswa) : retrofit2.Response<Void>
 }
 
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+) : RepositoryDataSiswa {
+    override suspend fun getDataSiswa() : List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa) : retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+}
